f5970e00e56f18da18a6612764de4201143eaf4e
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-19 17:40:53 -0300

    bump to 0.5.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "JSONMediaType",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 5)
     ]
 )
